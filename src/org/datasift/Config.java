340adb85fc045a95c1fd9846c2d224506a6fe0ec
Jessica Long <jessicalong@Jessicas-MacBook-Air.local>
2014-08-14 16:34:46 -0700

    configure datasift-java for use with Idibon

--- src/org/datasift/Config.java
+++ src/org/datasift/Config.java
@@ -7,6 +7,6 @@ package org.datasift;
  * This configuration is used by the examples and unit tests.
  */
 final public class Config {
-	public static String username = "<your_username>";
-	public static String api_key = "<your_api_key>";
+	public static String username = "idibon";
+	public static String api_key = "d0cd68978f4e90c42886f9f2f8378eef";
 }
